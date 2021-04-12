84e16bda3b4b869518721e76592e25645daeca6b
Noel Tesaluna <ntesaluna03@gmail.com>
2021-04-12 22:42:34 +1000

    Part 3 of the SAC, final paint application

---
+++ Part 3/paintSac/paintSac/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPen = New System.Windows.Forms.RadioButton()
+        Me.btnRectangle = New System.Windows.Forms.RadioButton()
+        Me.picCanvas = New System.Windows.Forms.PictureBox()
+        Me.btnClear = New System.Windows.Forms.RadioButton()
+        CType(Me.picCanvas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnPen
+        '
+        Me.btnPen.AutoSize = True
+        Me.btnPen.Location = New System.Drawing.Point(12, 12)
+        Me.btnPen.Name = "btnPen"
+        Me.btnPen.Size = New System.Drawing.Size(45, 17)
+        Me.btnPen.TabIndex = 0
+        Me.btnPen.TabStop = True
+        Me.btnPen.Text = "Line"
+        Me.btnPen.UseVisualStyleBackColor = True
+        '
+        'btnRectangle
+        '
+        Me.btnRectangle.AutoSize = True
+        Me.btnRectangle.Location = New System.Drawing.Point(73, 12)
+        Me.btnRectangle.Name = "btnRectangle"
+        Me.btnRectangle.Size = New System.Drawing.Size(74, 17)
+        Me.btnRectangle.TabIndex = 1
+        Me.btnRectangle.TabStop = True
+        Me.btnRectangle.Text = "Rectangle"
+        Me.btnRectangle.UseVisualStyleBackColor = True
+        '
+        'picCanvas
+        '
+        Me.picCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picCanvas.Location = New System.Drawing.Point(12, 35)
+        Me.picCanvas.Name = "picCanvas"
+        Me.picCanvas.Size = New System.Drawing.Size(776, 403)
+        Me.picCanvas.TabIndex = 2
+        Me.picCanvas.TabStop = False
+        '
+        'btnClear
+        '
+        Me.btnClear.AutoSize = True
+        Me.btnClear.Location = New System.Drawing.Point(154, 13)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(49, 17)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.TabStop = True
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.picCanvas)
+        Me.Controls.Add(Me.btnRectangle)
+        Me.Controls.Add(Me.btnPen)
+        Me.Name = "Form1"
+        Me.Text = "Paint"
+        CType(Me.picCanvas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPen As RadioButton
+    Friend WithEvents btnRectangle As RadioButton
+    Friend WithEvents picCanvas As PictureBox
+    Friend WithEvents btnClear As RadioButton
+End Class
---
+++ Part 3/paintSac/paintSac/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Dim mMouseDown As Boolean
+    Dim X1, Y1 As Integer
+    Dim oPen As Pen
+    Dim g As Graphics
+    Dim boolLine As Boolean
+    '----------------------------------------------------------
+    'Form1_Load
+    '   Loads the form and declares the graphics to the canvas
+    '----------------------------------------------------------
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        oPen = New Pen(Color.Black) 'Makes the pen black
+        g = Me.picCanvas.CreateGraphics 'Sets up the graphics
+    End Sub
+    '-----------------------------------------------------
+    'MouseDownEvent
+    '   When the mouse is pressed it starts to draw a line
+    '   or rectangle
+    '-----------------------------------------------------
+    Private Sub MouseDownEvent(s As Object, e As MouseEventArgs) Handles picCanvas.MouseDown
+        If e.Button = MouseButtons.Left Then 'If user presses left click then
+            If boolLine = True Then 'If the boolTest is true
+                mMouseDown = True 'Sets mMouseDown as true
+                X1 = e.X 'Current position is now the previous
+                Y1 = e.Y
+                g.DrawLine(oPen, X1, Y1, e.X, e.Y) 'Draws from previous to current position
+            End If
+            If boolLine = False Then 'If the boolTest is false
+                g.DrawRectangle(oPen, X1, Y1, e.X - X1, e.Y - Y1) 'Draws from previous to current position
+                X1 = e.X 'Current position is now the previous
+                Y1 = e.Y
+            End If
+        End If
+    End Sub
+    '-----------------------------------------------------
+    'MouseMoveEvent
+    '   When the mouse is moved and mouse is pressed it
+    '   draws a line or rectangle
+    '-----------------------------------------------------
+    Private Sub MouseMoveEvent(s As Object, e As MouseEventArgs) Handles picCanvas.MouseMove
+        If mMouseDown Then 'If mMouseDown is true
+            If boolLine = True Then 'If the boolTest is true
+                g.DrawLine(oPen, X1, Y1, e.X, e.Y) 'Draws from previous to current position
+                X1 = e.X 'Current position is now the previous
+                Y1 = e.Y
+                If boolLine = False Then 'If the boolTest is false
+
+
+                    g.DrawRectangle(oPen, X1, Y1, e.X - X1, e.Y - Y1) 'Draws from previous to current position
+                    X1 = e.X 'Current position is now the previous
+                    Y1 = e.Y
+                End If
+            End If
+        End If
+    End Sub
+    '---------------------------------------------------------
+    'MouseUPEvent
+    '   When the mouse not pressed it sets mMouseDown as false
+    '---------------------------------------------------------
+    Private Sub MouseUpEvent(s As Object, e As MouseEventArgs) Handles picCanvas.MouseUp
+        mMouseDown = False
+    End Sub
+    '---------------------------------------------------------
+    'BtnPen_CheckedChanged
+    '   When click it sets boolLine as True to be used in
+    '   the other subroutines
+    '---------------------------------------------------------
+    Private Sub BtnPen_CheckedChanged(sender As Object, e As EventArgs) Handles btnPen.CheckedChanged
+        boolLine = True
+
+    End Sub
+    '---------------------------------------------------------
+    'BtnRectangle_CheckedChanged
+    '   When click it sets boolLine as False to be used in
+    '   the other subroutines
+    '---------------------------------------------------------
+    Private Sub BtnRectangle_CheckedChanged(sender As Object, e As EventArgs) Handles btnRectangle.CheckedChanged
+        boolLine = False
+    End Sub
+    '---------------------------------------------------------
+    'BtnClear_CheckedChanged
+    '   When click it sets clears everything
+    '---------------------------------------------------------
+    Private Sub BtnClear_CheckedChanged(sender As Object, e As EventArgs) Handles btnClear.CheckedChanged
+        g.Clear(Color.White)
+    End Sub
+
+
+End Class
